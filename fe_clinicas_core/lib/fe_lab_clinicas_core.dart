a45de795aa422c0808d1950fea1650e3cf2713eb
Samuel A Fernandes <zamuelfernandes@gmail.com>
2024-01-31 17:46:01 -0300

    Sistema de mensagens

--- fe_clinicas_core/lib/fe_lab_clinicas_core.dart
+++ fe_clinicas_core/lib/fe_lab_clinicas_core.dart
@@ -1,1 +1,6 @@
 library fe_lab_clinicas_core;
+
+export 'src/helpers/messages.dart';
+export 'src/fp/either.dart';
+export 'src/fp/unit.dart';
+export 'src/fp/nil.dart';
--- fe_clinicas_core/lib/src/helpers/messages.dart
+++ fe_clinicas_core/lib/src/helpers/messages.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:signals_flutter/signals_flutter.dart';
 import 'package:top_snackbar_flutter/custom_snack_bar.dart';
 import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
@@ -22,5 +23,63 @@ final class Messages {
       Overlay.of(context),
       CustomSnackBar.success(message: message),
     );
+  }
+}
+
+mixin MessagesStateMixin {
+  final Signal<String?> _errorMessage = signal(null);
+
+  String? get errorMessage => _errorMessage();
+
+  final Signal<String?> _infoMessage = signal(null);
+
+  String? get infoMessage => _infoMessage();
+
+  final Signal<String?> _successMessage = signal(null);
+
+  String? get successMessage => _successMessage();
+
+  void clearError() => _errorMessage.value = null;
+  void clearInfo() => _infoMessage.value = null;
+  void clearSuccess() => _successMessage.value = null;
+
+  void showError(String message) {
+    untracked(() => clearError());
+    _errorMessage.value = message;
+  }
+
+  void showInfo(String message) {
+    untracked(() => clearInfo());
+    _infoMessage.value = message;
+  }
+
+  void showSuccess(String message) {
+    untracked(() => clearSuccess());
+    _successMessage.value = message;
+  }
+
+  void clearAllMessages() {
+    untracked(() {
+      clearError();
+      clearInfo();
+      clearSuccess();
+    });
+  }
+}
+
+mixin MessagesViewMixin<T extends StatefulWidget> on State<T> {
+  void messageListenner(MessagesStateMixin state) {
+    effect(() {
+      switch (state) {
+        case MessagesStateMixin(:final errorMessage?):
+          Messages.showError(errorMessage, context);
+
+        case MessagesStateMixin(:final infoMessage?):
+          Messages.showInfo(infoMessage, context);
+
+        case MessagesStateMixin(:final successMessage?):
+          Messages.showSuccess(successMessage, context);
+      }
+    });
   }
 }
