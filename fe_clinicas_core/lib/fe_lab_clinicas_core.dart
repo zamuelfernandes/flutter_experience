3e3cc348b92e38b6fab85a2b494deb8a90dd1273
Samuel A Fernandes <zamuelfernandes@gmail.com>
2024-01-31 18:46:28 -0300

    Flutter GetIt configure

--- fe_clinicas_core/lib/fe_lab_clinicas_core.dart
+++ fe_clinicas_core/lib/fe_lab_clinicas_core.dart
@@ -4,3 +4,6 @@ export 'src/helpers/messages.dart';
 export 'src/fp/either.dart';
 export 'src/fp/unit.dart';
 export 'src/fp/nil.dart';
+export 'src/restClient/rest_client.dart';
+export 'src/constants/local_storage_constants.dart';
+export 'src/fe_lab_clinicas_core_config.dart';
