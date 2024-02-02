18831020059205eec3235b9adf351bc23d630895
Samuel A Fernandes <zamuelfernandes@gmail.com>
2024-02-02 09:46:19 -0300

    Splash PAge

--- fe_clinicas_self_service/lib/main.dart
+++ fe_clinicas_self_service/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 
+import 'src/pages/splash_page/splash_page.dart';
+
 void main() {
   runApp(const LabClinicasSelfServiceApp());
 }
@@ -15,7 +17,7 @@ class LabClinicasSelfServiceApp extends StatelessWidget {
       title: 'Lab Clinicas Auto Atendimento',
       pagesBuilders: [
         FlutterGetItPageBuilder(
-          page: (_) => const MyHomePage(title: 'Start Test'),
+          page: (_) => const SplashPage(),
           path: '/',
         ),
       ],
